48bb575ffed550d653cbbaac7d751257a2055fb2
ToshmatovDeveloper <toshmatovdeveloper1105@gmail.com>
2026-06-23 10:39:03 +0500

    Added SwaggerUI

--- BookingService/src/BookingService.Domain/Entities/Booking.cs
+++ BookingService/src/BookingService.Domain/Entities/Booking.cs
@@ -9,6 +9,8 @@ public class Booking(Guid id, Guid hotelId, Guid roomId, DateTime startDate, Dat
     public Guid HotelId { get; set; } = hotelId;
 
     public Guid RoomId { get; set; } = roomId;
+    
+    public Room Room { get; set; }
 
     public DateTime StartDate { get; set; } = startDate;
 
--- BookingService/src/BookingService.Web/Program.cs
+++ BookingService/src/BookingService.Web/Program.cs
@@ -1,3 +1,4 @@
+using BookingService.Application.Hotel.Create;
 using BookingService.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,12 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 
 builder.Services.AddMediatR(cfg =>
 {
-    cfg.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    cfg.RegisterServicesFromAssembly(typeof(CreateHotelRequest).Assembly);
 });
 
 var app = builder.Build();
@@ -19,6 +21,9 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    
+    app.UseSwaggerUI(options => { options.SwaggerEndpoint("/openapi/v1.json", "BookingService"); });
 }
 
+app.MapControllers();
 app.Run();

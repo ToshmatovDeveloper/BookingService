2048b305407ea18800c01df55c86116d57c8c96a
Iqbol Toshmatov <toshmatovdeveloper1105@gmail.com>
2026-07-15 15:10:49 +0500

    Add JWT authentication with user login functionality and configuration

---
+++ BookingService/auth/AuthService.Application/CustomException/UnauthorizedException.cs
@@ -0,0 +1,3 @@
+﻿namespace AuthService.Application.CustomException;
+
+public class UnauthorizedException(string message) : Exception(message);
---
+++ BookingService/auth/AuthService.Application/Settings/JwtSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthService.Application.Settings;
+
+public class JwtSettings
+{
+    public const string SectionName = "JwtSettings";
+
+    public string Secret { get; set; } = null!;
+    public string Issuer { get; set; } = null!;
+    public string Audience { get; set; } = null!;
+    public int ExpirationInMinutes { get; set; }
+}
